ddea4d959b75b88a3f5b5a6f3926a103bdc4736f
Aruma Sanjayani <116952956+ArumaSanjayani@users.noreply.github.com>
2023-02-28 03:06:37 +0700

    Create nibiru.sh

---
+++ Nibiru Chain/nibiru.sh
@@ -0,0 +1,163 @@
+#!/bin/bash
+clear
+
+echo -e '\e[36mGarapan      :\e[39m' Nibiru Chain
+echo -e '\e[36mAuthor       :\e[39m' Aruma
+echo -e '\e[36mWebsite      :\e[39m' https://www.belajarcrypto.tech/
+echo "==================================================================" 
+
+sleep 2
+
+# Variable
+SOURCE=nibiru
+WALLET=wallet
+BINARY=nibid
+CHAIN=nibiru-itn-1
+FOLDER=.nibid
+VERSION=v0.19.2
+DENOM=unibi
+COSMOVISOR=cosmovisor
+REPO=https://github.com/NibiruChain/nibiru.git
+GENESIS=https://snapshots.kjnodes.com/nibiru-testnet/genesis.json
+ADDRBOOK=https://snapshots.kjnodes.com/nibiru-testnet/addrbook.json
+PORT=39
+
+echo "export SOURCE=${SOURCE}" >> $HOME/.bash_profile
+echo "export WALLET=${WALLET}" >> $HOME/.bash_profile
+echo "export BINARY=${BINARY}" >> $HOME/.bash_profile
+echo "export DENOM=${DENOM}" >> $HOME/.bash_profile
+echo "export CHAIN=${CHAIN}" >> $HOME/.bash_profile
+echo "export FOLDER=${FOLDER}" >> $HOME/.bash_profile
+echo "export VERSION=${VERSION}" >> $HOME/.bash_profile
+echo "export COSMOVISOR=${COSMOVISOR}" >> $HOME/.bash_profile
+echo "export REPO=${REPO}" >> $HOME/.bash_profile
+echo "export GENESIS=${GENESIS}" >> $HOME/.bash_profile
+echo "export ADDRBOOK=${ADDRBOOK}" >> $HOME/.bash_profile
+echo "export PORT=${PORT}" >> $HOME/.bash_profile
+source $HOME/.bash_profile
+
+# set vars
+if [ ! $NODENAME ]; then
+	read -p "Enter node name: " NODENAME
+	echo 'export NODENAME='$NODENAME >> $HOME/.bash_profile
+fi
+NIBIRU_PORT=39
+if [ ! $WALLET ]; then
+	echo "export WALLET=wallet" >> $HOME/.bash_profile
+fi
+echo "export CHAIN_ID=nibiru-itn-1" >> $HOME/.bash_profile
+echo "export NIBIRU_PORT=${NIBIRU_PORT}" >> $HOME/.bash_profile
+source $HOME/.bash_profile
+
+echo "==================================================================" 
+echo -e "Your node name     : \e[1m\e[32m$NODENAME\e[0m"
+echo -e "Your wallet name   : \e[1m\e[32m$WALLET\e[0m"
+echo -e "Your chain name    : \e[1m\e[32m$CHAIN_ID\e[0m"
+echo -e "Your port          : \e[1m\e[32m$NIBIRU_PORT\e[0m"
+echo "==================================================================" 
+
+sleep 2
+
+# Package
+sudo apt -q update
+sudo apt -qy install curl git jq lz4 build-essential
+sudo apt -qy upgrade
+
+# Install GO
+sudo rm -rf /usr/local/go
+curl -Ls https://go.dev/dl/go1.19.6.linux-amd64.tar.gz | sudo tar -xzf - -C /usr/local
+eval $(echo 'export PATH=$PATH:/usr/local/go/bin' | sudo tee /etc/profile.d/golang.sh)
+eval $(echo 'export PATH=$PATH:$HOME/go/bin' | tee -a $HOME/.profile)
+
+# Get testnet version of Nolus
+rm -rf nibiru
+git clone $REPO
+cd nibiru
+git switch -c v0.19.2
+
+# Build binaries
+cd $HOME/nibiru
+make build
+
+# Prepare binaries for Cosmovisor
+cd $HOME/nibiru
+mkdir -p $HOME/.nibid/cosmovisor/genesis/bin
+mv build/nibid $HOME/.nibid/cosmovisor/genesis/bin/
+rm -rf build
+
+# Create application symlinks
+cd $HOME/nibiru
+ln -s $HOME/.nibid/cosmovisor/genesis $HOME/.nibid/cosmovisor/current
+sudo ln -s $HOME/.nibid/cosmovisor/current/bin/nibid /usr/local/bin/nibid
+
+# Download and install Cosmovisor
+go install cosmossdk.io/tools/cosmovisor/cmd/cosmovisor@v1.4.0
+
+# Create service
+sudo tee /etc/systemd/system/nibid.service > /dev/null << EOF
+[Unit]
+Description=nibiru-testnet node service
+After=network-online.target
+
+[Service]
+User=$USER
+ExecStart=$(which cosmovisor) run start
+Restart=on-failure
+RestartSec=10
+LimitNOFILE=65535
+Environment="DAEMON_HOME=$HOME/.nibid"
+Environment="DAEMON_NAME=nibid"
+Environment="UNSAFE_SKIP_BACKUP=true"
+Environment="PATH=/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/usr/local/games:/snap/bin:$HOME/.nibid/cosmovisor/current/bin"
+
+[Install]
+WantedBy=multi-user.target
+EOF
+sudo systemctl daemon-reload
+sudo systemctl enable nibid
+
+# Set node configuration
+nibid config chain-id nibiru-itn-1
+nibid config keyring-backend test
+nibid config node tcp://localhost:39657
+
+# Download genesis and addrbook
+curl -Ls https://snapshots.kjnodes.com/nibiru-testnet/genesis.json > $HOME/.nibid/config/genesis.json
+curl -Ls https://snapshots.kjnodes.com/nibiru-testnet/addrbook.json > $HOME/.nibid/config/addrbook.json
+
+# Add seeds
+sed -i -e "s|^seeds *=.*|seeds = \"3f472746f46493309650e5a033076689996c8881@nibiru-testnet.rpc.kjnodes.com:39659\"|" $HOME/.nibid/config/config.toml
+
+# Set minimum gas price
+sed -i -e "s|^minimum-gas-prices *=.*|minimum-gas-prices = \"0.025unibi\"|" $HOME/.nibid/config/app.toml
+
+# Set pruning
+sed -i \
+  -e 's|^pruning *=.*|pruning = "custom"|' \
+  -e 's|^pruning-keep-recent *=.*|pruning-keep-recent = "100"|' \
+  -e 's|^pruning-keep-every *=.*|pruning-keep-every = "0"|' \
+  -e 's|^pruning-interval *=.*|pruning-interval = "19"|' \
+  $HOME/.nibid/config/app.toml
+
+# Set custom ports
+sed -i -e "s%^proxy_app = \"tcp://127.0.0.1:26658\"%proxy_app = \"tcp://127.0.0.1:39658\"%; s%^laddr = \"tcp://127.0.0.1:26657\"%laddr = \"tcp://127.0.0.1:39657\"%; s%^pprof_laddr = \"localhost:6060\"%pprof_laddr = \"localhost:39060\"%; s%^laddr = \"tcp://0.0.0.0:26656\"%laddr = \"tcp://0.0.0.0:39656\"%; s%^prometheus_listen_addr = \":26660\"%prometheus_listen_addr = \":39660\"%" $HOME/.nibid/config/config.toml
+sed -i -e "s%^address = \"tcp://0.0.0.0:1317\"%address = \"tcp://0.0.0.0:39317\"%; s%^address = \":8080\"%address = \":39080\"%; s%^address = \"0.0.0.0:9090\"%address = \"0.0.0.0:39090\"%; s%^address = \"0.0.0.0:9091\"%address = \"0.0.0.0:39091\"%; s%^address = \"0.0.0.0:8545\"%address = \"0.0.0.0:39545\"%; s%^ws-address = \"0.0.0.0:8546\"%ws-address = \"0.0.0.0:39546\"%" $HOME/.nibid/config/app.toml
+
+# Enable snapshots
+curl -L https://snapshots.kjnodes.com/nibiru-testnet/snapshot_latest.tar.lz4 | tar -Ilz4 -xf - -C $HOME/.nibid
+[[ -f $HOME/.nibid/data/upgrade-info.json ]] && cp $HOME/.nibid/data/upgrade-info.json $HOME/.nibid/cosmovisor/genesis/upgrade-info.json
+
+# Register And Start Service
+sudo systemctl start $BINARY
+sudo systemctl daemon-reload
+sudo systemctl enable $BINARY
+sudo systemctl restart nibid
+source $HOME/.bash_profile
+
+echo '=============== Installasi Kelar Gan ==================='
+
+echo -e "CHECK STATUS BINARY : \e[1m\e[35msystemctl status $BINARY\e[0m"
+echo -e "CHECK RUNNING LOGS : \e[1m\e[35msudo journalctl -u nibid -f --no-hostname -o cat\e[0m"
+echo -e "CHECK SYNC STATUS : \e[1m\e[35mnibid status 2>&1 | jq .SyncInfo\e[0m"
+
+# End
